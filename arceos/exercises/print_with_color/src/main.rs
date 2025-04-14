741e5b137498ae05d5ada5926db46fa05bcf07c1
xxxuuu <xxuuu0@outlook.com>
2025-04-14 22:16:05 +0800

    complete the exercises/print_with_color
    Signed-off-by: xxxuuu <xxuuu0@outlook.com>

--- arceos/exercises/print_with_color/src/main.rs
+++ arceos/exercises/print_with_color/src/main.rs
@@ -3,6 +3,12 @@
 
 #[cfg(feature = "axstd")]
 use axstd::println;
+#[cfg(feature = "axstd")]
+macro_rules! println {
+    ($($arg:tt)*) => {
+        axstd::println!("\x1b[34m{}\x1b[0m", format_args!($($arg)*));
+    };
+}
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
